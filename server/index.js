99862066a6bf1a98dd506268d747087dec58115e
hasan <hsanromadon@gmail.com>
2022-04-18 21:01:40 +0700

    add heroku configuration

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
   },
 });
 require('./src/socket')(io);
 
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
